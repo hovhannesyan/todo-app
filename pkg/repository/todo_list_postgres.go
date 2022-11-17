3800a5be62d81031d70fc60febb431f96b1fc177
hovhannesyan <arthurhovhannesyan@yahoo.com>
2022-11-17 03:06:53 +0100

    Added logic for lists

--- pkg/repository/todo_list_postgres.go
+++ pkg/repository/todo_list_postgres.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"github.com/hovhannesyan/todo-app"
 	"github.com/jmoiron/sqlx"
+	"github.com/sirupsen/logrus"
+	"strings"
 )
 
 type TodoListPostgres struct {
@@ -53,4 +55,41 @@ func (r *TodoListPostgres) GetById(userId, listId int) (todo.TodoList, error) {
 	err := r.db.Get(&list, query, userId, listId)
 
 	return list, err
+}
+
+func (r *TodoListPostgres) Update(userId, listId int, input todo.UpdateListInput) error {
+	setValues := make([]string, 0)
+	args := make([]interface{}, 0)
+	argId := 1
+
+	if input.Title != nil {
+		setValues = append(setValues, fmt.Sprintf("title=$%d", argId))
+		args = append(args, *input.Title)
+		argId++
+	}
+
+	if input.Description != nil {
+		setValues = append(setValues, fmt.Sprintf("description=$%d", argId))
+		args = append(args, *input.Description)
+		argId++
+	}
+
+	setQuery := strings.Join(setValues, ", ")
+
+	query := fmt.Sprintf("update %s tl set %s from %s ul where tl.id = ul.list_id and ul.list_id = $%d and ul.user_id = $%d", todoListsTable, setQuery, usersListsTable, argId, argId+1)
+	args = append(args, listId, userId)
+
+	logrus.Debugf("updateQuery: %s", query)
+	logrus.Debugf("args: %s", args)
+
+	_, err := r.db.Exec(query, args...)
+	return err
+
+}
+
+func (r *TodoListPostgres) Delete(userId, listId int) error {
+	query := fmt.Sprintf("delete from %s tl using %s ul where tl.id = ul.list_id and ul.user_id = $1 and ul.list_id = $2", todoListsTable, usersListsTable)
+	_, err := r.db.Exec(query, userId, listId)
+	return err
+
 }
